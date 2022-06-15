91edbeb3c186933b83a789db2a1b67fbaca59f13
josemiguelhseptien <josemiguelhseptien@gmail.com>
2022-06-15 01:41:16 +0000

    save to submit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,49 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("responsible").innerHTML = refreshString(
+    responsibles
+  );
+  document.getElementById("action").innerHTML = refreshString(actions);
+  document.getElementById("object").innerHTML = refreshString(object);
+  document.getElementById("time").innerHTML = refreshString(time);
 };
+
+let responsibles = [
+  "Aunt Jemima",
+  "Slash",
+  "Team Rocket",
+  "The Denver Broncos",
+  "Popeye",
+  "Superman"
+];
+let actions = [
+  "defeated",
+  "played",
+  "captured",
+  "fought",
+  "cooked for",
+  "tickled"
+];
+let object = [
+  "Pikachu",
+  "Batman",
+  "a samurai",
+  "my pre-K teacher",
+  "the Pope",
+  "bugs bunny",
+  "Spongebob"
+];
+let time = [
+  "yesterday",
+  "this morning",
+  "before the entire school",
+  "an hour ago",
+  "2 years ago and everyone still remembers",
+  "in my dreams, what do you think it means Doctor?"
+];
+
+function refreshString(arr) {
+  let randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
